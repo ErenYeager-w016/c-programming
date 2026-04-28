2865c6594bde10c2aaba632decf00ceedaf4c297
Milan Bairagi <nmilan626@gmail.com>
2026-04-28 12:32:31 +0545

    refactor: delete 1.19 and rename 1.20 to 1.19

--- Lab 1/1.19-lab.c
+++ Lab 1/1.19-lab.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    float r;
-    const float pi = 3.14159;
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
     
-    printf("Enter radius of sphere: ");
-    scanf("%f", &r);
-    
-    float volume = (4.0 / 3.0) * pi * r * r * r;
-    printf("Volume of sphere: %.2f\n", volume);
+    printf("Remainder after dividing by 5: %d\n", num % 5);
     
     printf("\nProgrammed by Kshitij Nepali\n");
     return 0;
